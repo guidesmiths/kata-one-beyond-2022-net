ce383f273cc189de00769fc8bf351df55065fcd9
César Demicheli <cesar.demicheli@dcsl.com>
2022-09-07 08:49:30 +0200

    CHORE: Initial commit

---
+++ KataNet/MorseDecoder.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace KataNet;
+
+[RankColumn]
+public abstract class MorseDecoder
+{
+	public static string MisteryMessage => ".-- .... .- -   .... .- - ....   --. --- -..   .-- .-. --- ..- --. .... -";
+
+	[Benchmark]
+	public abstract string DecodeMisteryMessage();
+}
---
+++ KataNet/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using KataNet;
+using System.Reflection;
+
+Console.WriteLine("Initializing...");
+
+var types = Assembly.GetExecutingAssembly()
+					.GetTypes()
+					.Where(x => x.IsSubclassOf(typeof(MorseDecoder)))
+					.ToArray();
+
+var instances = types.Select(x => (MorseDecoder)Activator.CreateInstance(x)!)
+					 .ToArray();
+
+var config = ManualConfig.Create(DefaultConfig.Instance)
+						 .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest))
+						 .WithOptions(ConfigOptions.JoinSummary | ConfigOptions.DisableLogFile);
+
+var benchmarks = types.Select(x => BenchmarkConverter.TypeToBenchmarks(x, config))
+					  .ToArray();
+
+BenchmarkRunner.Run(benchmarks);
+
+Console.WriteLine("\r\nDecoders results:");
+foreach (var decoder in instances)
+	Console.WriteLine($"{decoder.GetType().Name} => {decoder.DecodeMisteryMessage()}");
