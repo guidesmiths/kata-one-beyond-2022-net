208b0df87cd9646db51640ed9b8a15ec6d5ff701
Gonzalo Fernández Moya <gonzalo.fernandez@dcsl.com>
2022-09-29 17:17:07 +0200

    FEAT: kata otter

---
+++ KataNet/FinalMorseDecoder.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace KataNet;
+
+[RankColumn]
+public class FinalMorseDecoder : MorseDecoder
+{
+	[Benchmark]
+	public override string DecodeMisteryMessage()
+	{
+		var dictionary = GetMorseDictionary();
+
+		var sb = new StringBuilder();
+		var keys = MorseDecoder.MisteryMessage.Split(' ');
+
+		for (int i = 0; i < keys.Length; i++)
+		{
+			if (keys[i] == " " && keys[i + 1] == " ")
+			{
+				sb.Append(dictionary.ElementAt(i).Value);
+				i++;
+				continue;
+			}
+
+			dictionary.TryGetValue(keys[i], out var u);
+			sb.Append(u);
+		}
+
+		return sb.ToString();
+	}
+
+	private Dictionary<string, char> GetMorseDictionary()
+	{
+		return new Dictionary<string, char>()
+		{
+			[".-"] = 'a',
+			["-..."] = 'b',
+			["-.-."] = 'c',
+			["-.."] = 'd',
+			["."] = 'e',
+			["..-."] = 'f',
+			["--."] = 'g',
+			["...."] = 'h',
+			[".."] = 'i',
+			[".---"] = 'j',
+			["-.-"] = 'k',
+			[".-.."] = 'l',
+			["--"] = 'm',
+			["-."] = 'n',
+			["---"] = 'o',
+			[".--."] = 'p',
+			["--.-"] = 'q',
+			[".-."] = 'r',
+			["..."] = 's',
+			["-"] = 't',
+			["..-"] = 'u',
+			["...-"] = 'v',
+			[".--"] = 'w',
+			["-..-"] = 'x',
+			["-.--"] = 'y',
+			["--.."] = 'z',
+			[".----"] = '1',
+			["..---"] = '2',
+			["...--"] = '3',
+			["....-"] = '4',
+			["....."] = '5',
+			["-...."] = '6',
+			["--..."] = '7',
+			["---.."] = '8',
+			["----."] = '9',
+			["-----"] = '0',
+			[" "] = ' '
+		};
+	}
+
+
+}
