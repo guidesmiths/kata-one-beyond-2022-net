2a22b495f96558631db465c6101a3b310d3f8721
Matthew Toghill <Matthew.Toghill@DCSLSoftware.com>
2022-09-29 16:56:44 +0100

    Implement morse decoder

---
+++ KataNet/Decoders/MattMorseDecoder.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace KataNet.Decoders;
+
+public class MattMorseDecoder : MorseDecoder
+{
+    [Benchmark]
+    public override string DecodeMisteryMessage()
+    {
+        return new string(MisteryMessage.Replace("   ", " | ").Split().Select(ParseMorseCodeChar).ToArray());
+    }
+
+    private static char ParseMorseCodeChar(string morseCode)
+    {
+        return morseCode switch
+        {
+            ".-" => 'a',
+            "-..." => 'b',
+            "-.-." => 'c',
+            "-.." => 'd',
+            "." => 'e',
+            "..-." => 'f',
+            "--." => 'g',
+            "...." => 'h',
+            ".." => 'i',
+            ".---" => 'j',
+            "-.-" => 'k',
+            ".-.." => 'l',
+            "--" => 'm',
+            "-." => 'n',
+            "---" => 'o',
+            ".--." => 'p',
+            "--.-" => 'q',
+            ".-." => 'r',
+            "..." => 's',
+            "-" => 't',
+            "..-" => 'u',
+            "...-" => 'v',
+            ".--" => 'w',
+            "-..-" => 'x',
+            "-.--" => 'y',
+            "--.." => 'z',
+            ".----" => '1',
+            "..---" => '2',
+            "...--" => '3',
+            "....-" => '4',
+            "....." => '5',
+            "-...." => '6',
+            "--..." => '7',
+            "---.." => '8',
+            "----." => '9',
+            "-----" => '0',
+            _ => ' '
+        };
+    }
+}
