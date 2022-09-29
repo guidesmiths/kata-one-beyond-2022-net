7ab7d4b919969662b8a912b24d43dcfe183443d3
Alberto <alberto_tureganocsb@hotmail.com>
2022-09-29 17:19:08 +0200

    TigerMorseDecoder.cs

---
+++ KataNet/Decoders/TigerMorseDecoder.cs
@@ -0,0 +1,55 @@
+using System.Text.RegularExpressions;
+
+namespace KataNet;
+
+public class TigerMorseDecoder : MorseDecoder
+{
+    private static Dictionary<string, char> Dictionary = new()
+    {
+        [".-"] = 'a',
+        ["-..."] = 'b',
+        ["-.-."] = 'c',
+        ["-.."] = 'd',
+        ["."] = 'e',
+        ["..-."] = 'f',
+        ["--."] = 'g',
+        ["...."] = 'h',
+        [".."] = 'i',
+        [".---"] = 'j',
+        ["-.-"] = 'k',
+        [".-.."] = 'l',
+        ["--"] = 'm',
+        ["-."] = 'n',
+        ["---"] = 'o',
+        [".--."] = 'p',
+        ["--.-"] = 'q',
+        [".-."] = 'r',
+        ["..."] = 's',
+        ["-"] = 't',
+        ["..-"] = 'u',
+        ["...-"] = 'v',
+        [".--"] = 'w',
+        ["-..-"] = 'x',
+        ["-.--"] = 'y',
+        ["--.."] = 'z',
+        [".----"] = '1',
+        ["..---"] = '2',
+        ["...--"] = '3',
+        ["....-"] = '4',
+        ["....."] = '5',
+        ["-...."] = '6',
+        ["--..."] = '7',
+        ["---.."] = '8',
+        ["----."] = '9',
+        ["-----"] = '0'
+    };
+
+    public override string DecodeMisteryMessage()
+    {
+        var item = Regex.Replace(MisteryMessage, "[.-]+", x => Dictionary[x.Value].ToString());
+
+        item = Regex.Replace(item, "(\\w) (\\w)", x => $"{x.Groups[1].Value}{x.Groups[2].Value}");
+        return Regex.Replace(item, "(\\w) (\\w)", x => $"{x.Groups[1].Value}{x.Groups[2].Value}").Replace("   ", " ");
+        
+    }
+}
